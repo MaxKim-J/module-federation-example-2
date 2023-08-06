45477919cb8431c0dbdb27a58b08c9f8ffff6f7d
Max Kim <hwaseen@gmail.com>
2023-08-06 16:51:28 +0900

    fix: dependency

--- mf-remote-2/webpack.config.js
+++ mf-remote-2/webpack.config.js
@@ -47,11 +47,7 @@ module.exports = {
         react: {
           singleton: true,
         },
-        'shared-deps-mf-package': {
-          singleton: true, // 먼저 로드된 공유 의존성 모듈의 버전이
-          requiredVersion: '>=1.0.8 <1.0.11', // 이 semver 버전 범위 안에 들어오지 않으면
-          // strictVersion: true, // 런타임에서 에러를 낸다.
-        },
+        'shared-deps-mf-package': {},
         'lodash.camelcase': {},
         'date-fns': {},
       },
